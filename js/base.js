47c0640ba9c571b559bb0efc14aa5649441246fe
John Zhu <hitzhzw3889@126.com>
2017-02-21 19:28:47 +0800

    添加及查看list之二

--- js/base.js
+++ js/base.js
@@ -1,32 +1,36 @@
 ;(function(){
     'use strict';
+
     var $form_add_task = $('.add-task');
-    var new_task = {};		// Object类型
-    var task_list = [];		// Array类型
+    var new_task = {};
+    var task_list = [];
 
     init();
     
     $form_add_task.on('submit', function(e){
-	// 禁止默认行为
+	// 禁用默认行为
 	e.preventDefault();
-	// 获取新task的值
-	new_task.content = $(this).find('input[name="content"]').val();
-	// 如果新task的值为空则直接返回，否则继续执行 
-	if( !new_task.content ) return;
-	// 存入新task,更新task-item
+	// 获取新task值
+	new_task.content = $(this).find('input[name=content]').val();
+	// 如新task值为空，则直接返回，否则继续执行
+	if( !new_task.content ){
+	    return;
+	}
+	// 存入新task值
 	if( add_task(new_task) ){
 	    render_task_list();
 	}
 
-	// console.log('new_task', new_task);
     });
 
     function add_task(new_task){
-	// 将新task推入task_list
+	// 将新task推入task-list
 	task_list.push(new_task);
 	// 更新localStorage
 	store.set('task_list', task_list);
-	// console.log('task_list', task_list);
+	// 用作if语句的布尔表达式
+	return true;
+	// console.log('task_list', task_list); 
     }
 
     function init(){
@@ -34,7 +38,24 @@
     }
 
     function render_task_list(){
-	console.log('1', 1);
+	var $task_list = $('.task-list');
+	for(var i = 0; i < task_list.length; i++){
+	    var $task = render_task_tpl(task_list[i]);
+	    $task_list.append($task);
+	}
     }
+
+    function render_task_tpl(data){
+	var list_item_tpl =
+	    '<div class="task-item">' + 
+	    '<span><input name="" type="checkbox" value=""/></span>' +
+	    '<span class="task-content">' + data.content + '</span>' +
+	    '<span>删除</span>' +
+	    '<span>详细</span>' +
+	    '</div>';
+	return $(list_item_tpl);
+    }
+
 })();//本地域
+
 
