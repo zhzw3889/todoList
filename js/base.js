0f445910560092feb3fba05e3637774fce568d4b
John Zhu <hitzhzw3889@126.com>
2017-02-21 22:34:56 +0800

    添加task done

--- js/base.js
+++ js/base.js
@@ -2,16 +2,20 @@
     'use strict';
 
     var $form_add_task = $('.add-task');
-    var new_task = {};
     var task_list = [];
 
     init();
     
     $form_add_task.on('submit', function(e){
+	// new_task必须在此域中，之前放在全局中出现问题
+	var new_task = {};
+	var $input;
+
 	// 禁用默认行为
 	e.preventDefault();
 	// 获取新task值
-	new_task.content = $(this).find('input[name=content]').val();
+	$input = $(this).find('input[name=content]');
+	new_task.content = $input.val();
 	// 如新task值为空，则直接返回，否则继续执行
 	if( !new_task.content ){
 	    return;
@@ -19,8 +23,8 @@
 	// 存入新task值
 	if( add_task(new_task) ){
 	    render_task_list();
+	    $input.val('');
 	}
-
     });
 
     function add_task(new_task){
@@ -30,15 +34,19 @@
 	store.set('task_list', task_list);
 	// 用作if语句的布尔表达式
 	return true;
-	// console.log('task_list', task_list); 
     }
 
     function init(){
 	task_list = store.get('task_list') || [];
+	if( task_list.length ){
+	    render_task_list();
+	}
     }
 
     function render_task_list(){
 	var $task_list = $('.task-list');
+	$task_list.html('');
+
 	for(var i = 0; i < task_list.length; i++){
 	    var $task = render_task_tpl(task_list[i]);
 	    $task_list.append($task);
@@ -50,12 +58,13 @@
 	    '<div class="task-item">' + 
 	    '<span><input name="" type="checkbox" value=""/></span>' +
 	    '<span class="task-content">' + data.content + '</span>' +
-	    '<span>删除</span>' +
-	    '<span>详细</span>' +
+	    '<span class="fr">' + 
+	    '<span class="action"> 删除</span>' +
+	    '<span class="action"> 详细</span>' +
+	    '</span>' +
 	    '</div>';
 	return $(list_item_tpl);
     }
-
 })();//本地域
 
 
