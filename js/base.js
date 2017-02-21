f57427e5c3e4e554421b8cfcfd5f9dd19bfc1630
John Zhu <hitzhzw3889@126.com>
2017-02-21 23:52:32 +0800

    删除task done

--- js/base.js
+++ js/base.js
@@ -2,6 +2,7 @@
     'use strict';
 
     var $form_add_task = $('.add-task');
+    var $delete_task;
     var task_list = [];
 
     init();
@@ -22,18 +23,47 @@
 	}
 	// 存入新task值
 	if( add_task(new_task) ){
-	    render_task_list();
-	    $input.val('');
+	    // render_task_list();
+	    $input.val('');	// null也可
 	}
     });
 
+    // 查找并监听所有删除按钮的点击事件
+    function listen_task_delete(){
+	$delete_task.on('click', function(){
+	    var $this = $(this);
+	    // 找到删除按钮所在的task元素
+	    var $item = $this.parent().parent();
+	    var index = $item.data('index');
+	    var tmp = confirm('确定删除?');
+	    
+	    return tmp ? delete_task(index) : null;
+	});
+    }
+    
     function add_task(new_task){
 	// 将新task推入task-list
 	task_list.push(new_task);
 	// 更新localStorage
-	store.set('task_list', task_list);
+	refresh_task_list();
 	// 用作if语句的布尔表达式
 	return true;
+    }
+
+    // 刷新localStorage并渲染模板(tpl)
+    function refresh_task_list(){
+	store.set('task_list', task_list);
+	render_task_list();
+    }
+
+    // 删除一条task
+    function delete_task(index){
+	// 如果没有index或者index不存在，则直接返回
+	if( index === undefined || !task_list[index] ){
+	    return;
+	}
+	delete task_list[index];
+	refresh_task_list();
     }
 
     function init(){
@@ -43,23 +73,30 @@
 	}
     }
 
+    // 渲染所有task模板
     function render_task_list(){
 	var $task_list = $('.task-list');
 	$task_list.html('');
-
 	for(var i = 0; i < task_list.length; i++){
-	    var $task = render_task_tpl(task_list[i]);
+	    var $task = render_task_item(task_list[i], i);
 	    $task_list.append($task);
 	}
+	$delete_task = $('.action.delete');
+	listen_task_delete();
     }
 
-    function render_task_tpl(data){
+    // 渲染单条task模板
+    function render_task_item(data, index){
+	if( !data || !index ){
+	    return;
+	}
+
 	var list_item_tpl =
-	    '<div class="task-item">' + 
+	    '<div class="task-item" data-index="' + index + '" >' + 
 	    '<span><input name="" type="checkbox" value=""/></span>' +
 	    '<span class="task-content">' + data.content + '</span>' +
 	    '<span class="fr">' + 
-	    '<span class="action"> 删除</span>' +
+	    '<span class="action delete"> 删除</span>' +
 	    '<span class="action"> 详细</span>' +
 	    '</span>' +
 	    '</div>';
